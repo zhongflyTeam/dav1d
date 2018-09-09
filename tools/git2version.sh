aab5188895f1d23ebf6585590972ae0575b098ec
Marvin Scholz <epirat07@gmail.com>
2018-09-09 17:39:41 +0200

    Buildsystem: Remove no longer needed git2version script
    This is done in a similar way directly in meson now.

--- tools/git2version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eu
-version=$(git show-ref --head --abbrev=8 | grep " HEAD" | head -n1 | cut -d" " -f1)
-tag=$(git show-ref --tags | grep "${version}" | cut -d"/" -f3 | cut -d" " -f2 | tr '_' '.')
-[ -z "${tag}" ] && tag="${version}"
-echo "/* auto-generated, do not edit */"
-echo "#define DAV1D_VERSION \"${tag}\""
