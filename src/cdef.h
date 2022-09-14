cfcdd23d166ad32af7e1bc43a5105a568f9651d1
Kyle Siefring <siekyleb@amazon.com>
2022-09-14 10:58:14 -0400

    Buffer cdef input in 8 block wide chunks
    Also, implement new CDEF for AVX2. The AVX2 implementation offsets the
    input pixels by 128 and interleaves the chroma planes.
    
    Profiling results from zen2.
    NEW:
    (Filters 2 chroma planes at once)
    cdef_filter_uv_4x4_01_8bpc_avx2: 102.2
    cdef_filter_uv_4x4_10_8bpc_avx2: 79.7
    cdef_filter_uv_4x4_11_8bpc_avx2: 199.6
    cdef_filter_uv_4x8_01_8bpc_avx2: 171.9
    cdef_filter_uv_4x8_10_8bpc_avx2: 128.3
    cdef_filter_uv_4x8_11_8bpc_avx2: 251.6
    cdef_filter_uv_8x8_01_8bpc_avx2: 294.7
    cdef_filter_uv_8x8_10_8bpc_avx2: 240.6
    cdef_filter_uv_8x8_11_8bpc_avx2: 436.2
    cdef_filter_y_01_8bpc_avx2: 188.9
    cdef_filter_y_10_8bpc_avx2: 112.8
    cdef_filter_y_11_8bpc_avx2: 241.6
    
    (Prepares the input buffer for cdef)
    cdef_prep_uv_4x4_8bpc_avx2: 60.0
    cdef_prep_uv_4x8_8bpc_avx2: 90.5
    cdef_prep_uv_8x8_8bpc_avx2: 126.0
    cdef_prep_y_8bpc_avx2: 81.6
    
    Runtime from a WebRTC sample clip (about ~10% spent in cdef):
      Time (mean ± σ):     881.6 ms ±   6.6 ms    [User: 873.3 ms, System: 6.7 ms]
      Range (min … max):   874.7 ms … 898.2 ms    20 runs
    
    OLD:
    cdef_filter_4x4_01_8bpc_avx2: 86.8
    cdef_filter_4x4_01_16bpc_avx2: 85.5
    cdef_filter_4x4_10_8bpc_avx2: 80.3
    cdef_filter_4x4_10_16bpc_avx2: 77.9
    cdef_filter_4x4_11_8bpc_avx2: 116.5
    cdef_filter_4x4_11_16bpc_avx2: 108.9
    cdef_filter_4x8_01_8bpc_avx2: 122.8
    cdef_filter_4x8_01_16bpc_avx2: 119.6
    cdef_filter_4x8_10_8bpc_avx2: 90.2
    cdef_filter_4x8_10_16bpc_avx2: 102.6
    cdef_filter_4x8_11_8bpc_avx2: 176.2
    cdef_filter_4x8_11_16bpc_avx2: 159.5
    cdef_filter_8x8_01_8bpc_avx2: 171.2
    cdef_filter_8x8_01_16bpc_avx2: 154.2
    cdef_filter_8x8_10_8bpc_avx2: 122.2
    cdef_filter_8x8_10_16bpc_avx2: 121.2
    cdef_filter_8x8_11_8bpc_avx2: 247.7
    cdef_filter_8x8_11_16bpc_avx2: 237.1
    
    Time (mean ± σ):     909.1 ms ±   4.8 ms    [User: 900.6 ms, System: 6.1 ms]
    Range (min … max):   902.5 ms … 921.7 ms    20 runs

--- src/cdef.h
+++ src/cdef.h
@@ -33,29 +33,67 @@
 
 #include "common/bitdepth.h"
 
+// Buffer size in units of cdef blocks (8x8 pixels for luma)
+#define CDEF_BUFFER_UNITS 8
+
+// Maximum required size of the cdef luma buffer
+#define CDEF_BUFFER_Y_SIZE ((CDEF_BUFFER_UNITS * 8 + 8 * 2) * 12)
+
+// Maximum required size of the cdef chroma buffer
+#define CDEF_BUFFER_UV_SIZE (2 * (CDEF_BUFFER_UNITS * 8 + 8 * 2) * 12)
+
 enum CdefEdgeFlags {
     CDEF_HAVE_LEFT = 1 << 0,
     CDEF_HAVE_RIGHT = 1 << 1,
     CDEF_HAVE_TOP = 1 << 2,
     CDEF_HAVE_BOTTOM = 1 << 3,
+
+    // Used to prepare cdef buffers
+    CDEF_LEFT_SKIP = 1 << 4,
 };
 
-#ifdef BITDEPTH
-typedef const pixel (*const_left_pixel_row_2px)[2];
-#else
-typedef const void *const_left_pixel_row_2px;
-#endif
+// Move luma pixel data from $src to $buffer. The format of data in buffer is
+// implementation defined for each pair of cdef_prep_y and cdef_y function.
+// CDEF operates entirely on pre-filter data; if the right edge is present
+// (according to $edges), then the prefilter data is located in $src. However,
+// the edge pixels left of, above, or below $src may be post-filter. In order
+// to get access to pre-filter top/bottom pixels, use $top and $bottom.
+// Accessing pixels to the left is different. In the case that the 8x64 wide
+// superblock row has been skipped (according to $edges), then the pixels are
+// obtained from src. If not, then the data is obtained from the data in the
+// buffer from the previous superblock row.
+#define decl_cdef_prep_y_fn(name) \
+void (name)(pixel *buffer, const pixel *src, ptrdiff_t src_stride, \
+            const pixel *top, const pixel *bottom, int num_blocks, \
+            enum CdefEdgeFlags edges)
+typedef decl_cdef_prep_y_fn(*cdef_prep_y_fn);
 
-// CDEF operates entirely on pre-filter data; if bottom/right edges are
-// present (according to $edges), then the pre-filter data is located in
-// $dst. However, the edge pixels above $dst may be post-filter, so in
-// order to get access to pre-filter top pixels, use $top.
-#define decl_cdef_fn(name) \
-void (name)(pixel *dst, ptrdiff_t stride, const_left_pixel_row_2px left, \
-            const pixel *top, const pixel *bottom, \
-            int pri_strength, int sec_strength, \
-            int dir, int damping, enum CdefEdgeFlags edges HIGHBD_DECL_SUFFIX)
-typedef decl_cdef_fn(*cdef_fn);
+// Move pixel data from both chroma planes ($src) to $buffer. The format of data
+// in buffer is implementation defined for each pair of cdef_prep_y and cdef_y
+// function.
+#define decl_cdef_y_fn(name) \
+void (name)(pixel *dst, ptrdiff_t stride, const pixel *buffer, ptrdiff_t cbx, \
+            int pri_strength, int sec_strength, int dir, int damping, \
+            enum CdefEdgeFlags edges HIGHBD_DECL_SUFFIX)
+typedef decl_cdef_y_fn(*cdef_y_fn);
+
+// Move pixel data from both chroma planes ($src) to $buffer. The format of data
+// in buffer is implementation defined for each pair of cdef_prep_uv and cdef_uv
+// function.
+#define decl_cdef_prep_uv_fn(name) \
+void (name)(pixel *buffer, pixel **src, ptrdiff_t src_stride, \
+            pixel **top, pixel **bottom, int num_blocks,      \
+            enum CdefEdgeFlags edges)
+typedef decl_cdef_prep_uv_fn(*cdef_prep_uv_fn);
+
+// Apply the chroma cdef filters on buffers at the $cbx th cdef block and the
+// results are stored in dst. The format of data in buffer is implementation
+// defined for each pair of cdef_prep_uv and cdef_uv function.
+#define decl_cdef_uv_fn(name) \
+void (name)(pixel **dst, ptrdiff_t stride, const pixel *buffer, ptrdiff_t cbx, \
+            int pri_strength, int sec_strength, int dir, int damping, \
+            enum CdefEdgeFlags edges HIGHBD_DECL_SUFFIX)
+typedef decl_cdef_uv_fn(*cdef_uv_fn);
 
 #define decl_cdef_dir_fn(name) \
 int (name)(const pixel *dst, ptrdiff_t dst_stride, unsigned *var HIGHBD_DECL_SUFFIX)
@@ -63,7 +101,14 @@ typedef decl_cdef_dir_fn(*cdef_dir_fn);
 
 typedef struct Dav1dCdefDSPContext {
     cdef_dir_fn dir;
-    cdef_fn fb[3 /* 444/luma, 422, 420 */];
+
+    // The buffer storage format of prep_y and fb_y must match
+    cdef_prep_y_fn prep_y;
+    cdef_y_fn fb_y;
+
+    // The buffer storage format of prep_y, fb_y, uv_estride must match.
+    cdef_prep_uv_fn prep_uv[3 /* 444/luma, 422, 420 */];
+    cdef_uv_fn fb_uv[3 /* 444/luma, 422, 420 */];
 } Dav1dCdefDSPContext;
 
 bitfn_decls(void dav1d_cdef_dsp_init, Dav1dCdefDSPContext *c);
