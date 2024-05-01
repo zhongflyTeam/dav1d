715b2d443e42d907ea5e196b742c6606f809b4af
Luca Barbato <lu_zero@gentoo.org>
2024-05-01 12:58:42 +0000

    ppc: itx 8x8 pwr9

--- src/ppc/itx.h
+++ src/ppc/itx.h
@@ -58,5 +58,6 @@ static ALWAYS_INLINE void itx_dsp_init_ppc(Dav1dInvTxfmDSPContext *const c, cons
     assign_itx17_fn( ,  4,  4, pwr9);
     assign_itx16_fn(R,  4,  8, pwr9);
     assign_itx16_fn(R,  8,  4, pwr9);
+    assign_itx16_fn(,   8,  8, pwr9);
 #endif
 }
